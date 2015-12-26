c7b244ab8ac3af6a22af5bc86bb86d92726882af
olegmorales <olegmaroseev>
2015-12-27 01:32:32 +0300

    Доработано главное меню и его дизайн

--- mainMenu.hs
+++ mainMenu.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE  ExistentialQuantification, ScopedTypeVariables #-}
-
 module MainMenu where
 
 import Graphics.Gloss
@@ -11,16 +9,25 @@ level2min = "pic/level1.bmp"
 level3min = "pic/level1.bmp"
 level4min = "pic/level1.bmp"
 level5min = "pic/level1.bmp"
+butQuitMin = "pic/but.bmp"
+
 
 mainMenu :: IO()
-mainMenu = do
-        level1ico@(Bitmap _ _ _ _) <- loadBMP level1min
-        runGUI 
-         (InWindow "Tower Defence Main Menu" (600, 400) 
-         (100,  100))
-         (greyN 0.25) 
-         30
-         [("TestButton1", GUIElem (TextButton (0, 0) 100 50 (greyN 0.5) "START" False)), ("TestButton2", GUIElem (TextButton (200, 0) 100 50 (greyN 0.5) "QUIT" False)),
-         ("TestButton3", GUIElem (IconButton (100, 0) 150 150 level1ico))]
-         (\_ -> id)
-         updateAll--(\_ -> id)
+mainMenu = do 
+    level1ico@(Bitmap _ _ _ _) <- loadBMP level1min
+    butQuit@(Bitmap _ _ _ _) <- loadBMP butQuitMin
+    runGUI (InWindow "Tower Defence Main Menu" (600, 400) 
+     (100,  100))
+     (greyN 0.25) 
+     30
+     [("ButtonQuit", GUIElem (IconButton (0, -120) 100 50 butQuit False)),
+      ("Button1", GUIElem (IconButton (0, 120) 50 50 level1ico False)),
+      ("Button2", GUIElem (IconButton (-130, 120) 50 50 level1ico False)), 
+      ("Button3", GUIElem (IconButton (130, 120) 50 50 level1ico False)),
+      ("Button4", GUIElem (IconButton (70, 0) 50 50 level1ico False)),
+      ("Button5", GUIElem (IconButton (-70, 0) 50 50 level1ico False))]
+      (\_ -> id)
+      renderMainMenu
+
+renderMainMenu :: Float -> [(String, GUIElem)] -> [(String, GUIElem)]
+renderMainMenu time xs = xs
