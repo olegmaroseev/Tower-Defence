9f147d093d88ba539e31868d22ad4412430e68c3
theraconteur <raskin.anton.v@gmail.com>
2015-12-27 16:18:49 +0300

    Config файл

---
+++ Config.hs
@@ -0,0 +1,13 @@
+module Config where
+
+picBase = [("Tower1", "pic/tower1.bmp")
+          ,("Tower2", "pic/tower2.bmp")
+          ,("Tower3", "pic/tower3.bmp")
+          ,("Background1", "pic/background.bmp")
+          ]
+
+--Main Window params
+controlPanelHeight = 80 :: Int
+menuPanelHeight = 0 :: Int
+width = 1280 :: Int
+height = 720 :: Int
