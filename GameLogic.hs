926b073368cbb70d8e48874728d988a3e266eb51
Zolotarev FY <tedzolotarev@gmail.com>
2015-12-27 14:56:35 +0300

    Написал некоторые игровые события (установка новой башни, выделение башен)

--- GameLogic.hs
+++ GameLogic.hs
@@ -8,6 +8,7 @@ import Graphics.Gloss.Data.Vector
 import Graphics.Gloss.Data.Picture
 import Graphics.Gloss.Interface.Pure.Game
 import Data.Typeable
+import Data.Maybe
 
 type Tower = GameObject
 type Enemy = GameObject
@@ -30,6 +31,7 @@ data GameObject =
             path::[Point],
             speed::Float,
             hitpoints::Float,
+            power::Float,
             update::Float->[GameObject]->[GameObject]} |
   Bullet {  name::String,
             position::Point,
@@ -52,7 +54,9 @@ data GameState = GameState { level :: Level,
                    money :: Int,
                    isPaused :: Bool,
                    selectedTower :: String,
-                   objects :: [GameObject]
+                   lives :: Float,
+                   objects :: [GameObject],
+                   placingTower :: Maybe GameObject
                    }
                    
 data Game = Game Point   
@@ -68,27 +72,54 @@ instance GUIObject Game where
   
 --TODO: Actually use w and h parameters (Resize level? And everything else?)
 renderGame :: Game -> Picture
-renderGame (Game (x, y) w h GameState{..}) = Translate x y $ Pictures $ (levelP : objectsP)
+renderGame (Game (x, y) w h GameState{..}) = Translate x y $ Pictures $ (levelP : objectsP ++ placingP)
   where
     levelP = levelPicture level
-    objectsP = map render objects
+    objectsP = map render objects -- TODO: special render for selected tower
+    placingP = map render $ maybeToList placingTower 
 
-updateOrder :: GameObject -> Int
-updateOrder Tower{..} = 1
-updateOrder Enemy{..} = 2
-updateOrder Bullet{..} = 3
+objectsOrder :: GameObject -> Int
+objectsOrder Tower{..}  = 1
+objectsOrder Enemy{..}  = 2
+objectsOrder Bullet{..} = 3
 
 updateObjects :: Float -> [GameObject] -> [GameObject]
-updateObjects time xs = updated
-  where
-    sorted = sortBy (comparing updateOrder) xs
-    updated = foldl (\acc x -> update x time acc) xs xs
+updateObjects time xs = foldl (\acc x -> update x time acc) xs xs
     
 updateGame :: Float -> Game -> Game
-updateGame time (Game (x, y) w h gs@GameState{..}) = (Game (x, y) w h gs { objects = (updateObjects time objects)})
+updateGame time (Game (x, y) w h gs@GameState{..}) = (Game (x, y) w h gs { objects = globalUpdates})
+  where
+    individualUpdates = updateObjects time objects
+    globalUpdates = undefined -- TODO: wave spawning, check if enemy reached the end
 
---TODO: Tower select, something else?
+--TODO: Check if tower is on the path or collides with other towers
+isPlacementCorrect :: Point -> GameState -> Bool
+isPlacementCorrect pos GameState{..} = undefined
+
+distance :: Point -> Point -> Float
+distance (x1, y1) (x2, y2) = let dx = x2 - x1 
+                                 dy = y2 - y1 in sqrt (dx * dx + dy * dy)
+
+gameObjectHittest :: GameObject -> Point -> Bool
+gameObjectHittest t p = 20 > distance p (position t)
+
+isTower :: GameObject -> Bool
+isTower Tower{..} = True
+isTower _ = False
+
 handleGameEvents :: Event -> Game -> Game
-handleGameEvents event (Game (x, y) w h GameState{..}) = undefined
-
---TODO: Tower placement, tower upgrading, tower selling, pausing game
+handleGameEvents (EventMotion mpos) (Game (x, y) w h gs@GameState{..}) = (Game (x, y) w h gs { placingTower = newPlacingTower})
+  where
+    newPlacingTower = maybe Nothing (\t ->Just t {position = mpos}) placingTower
+handleGameEvents (EventKey (MouseButton LeftButton) Up _ pos) (Game (x, y) w h gs@GameState{..}) = (Game (x, y) w h gs { objects = newObjects, selectedTower = newSelectedName, placingTower = newPlacing})
+  where
+    place = isPlacementCorrect pos gs
+    possibleSelect = maybe selectedTower name $ find (\x -> gameObjectHittest x pos && isTower x) objects
+    newSelectedName = if isJust placingTower then selectedTower else possibleSelect
+    newObjects 
+      | place && isJust placingTower = sortBy (comparing objectsOrder) $ (fromJust placingTower) : objects
+      | otherwise = objects
+    newPlacing 
+      | place && isJust placingTower = Nothing
+      | otherwise = placingTower
+--TODO: tower upgrading, tower selling, pausing game
